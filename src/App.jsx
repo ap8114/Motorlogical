1f22531a8377008e17952daa7f6049c9e531e644
ap8114 <amanpatidar086@gmail.com>
2025-06-13 13:08:42 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+ 
+    </>
+  )
+}
+
+export default App
