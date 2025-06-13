bccbd4178fd332dbb70f02bbc7cecde16022e34f
ap8114 <amanpatidar086@gmail.com>
2025-06-13 16:11:14 +0530

    push

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -3,19 +3,40 @@ import { Outlet } from "react-router-dom";
 import Header from "../Layout/Header";
 import Sidebar from "../Layout/Sidebar";
 
+const MainLayout = () => {
+  return (
+    <div className="d-flex" style={{ minHeight: "100vh", overflow: "hidden" }}>
+      {/* Sidebar - fixed height and width */}
+      <div
+        style={{
+          width: "250px",
+          backgroundColor: "#0f2c43", // same dark blue
+          minHeight: "100vh",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          zIndex: 1000
+        }}
+      >
+        <Sidebar />
+      </div>
 
-const MainLayout = () => (
-  <div className="d-flex" style={{ minHeight: "100vh" }}>
-    <div>
-      <Sidebar />
-    </div>
-    <div className="flex-grow-1" style={{ background: "#f8fafc" }}>
-      <Header />
-      <div className="p-4">
-        <Outlet />
+      {/* Main Content Area */}
+      <div
+        className="flex-grow-1"
+        style={{
+          marginLeft: "250px", // equal to sidebar width
+          background: "#f8fafc",
+          minHeight: "100vh",
+        }}
+      >
+        <Header />
+        <div className="p-2">
+          <Outlet />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MainLayout;
