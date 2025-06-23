f5911e2f783c933c2ef9b7deb048e41b199a3dde
Rohit Patidar <ritikpatidar580@gmail.com>
2025-06-23 16:08:24 +0530

    rohit

--- src/Components/Dashboard/InventoryManagement.jsx
+++ src/Components/Dashboard/InventoryManagement.jsx
@@ -395,7 +395,7 @@ const InventoryManagement = () => {
             </button>
             <button
               onClick={() => setActiveTab("ordersystem")}
-              className={`whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm cursor-pointer !rounded-button ${activeTab === "order"
+              className={`whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm cursor-pointer !rounded-button ${activeTab === "ordersystem"
                   ? "border-indigo-500 text-indigo-600"
                   : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
                 }`}
